2d4480156a3c171cc2afaf649ead1416cfd9d31b
Ufuoma Isaac <ejemudaroufuoma@gmail.com>
2022-09-18 16:20:15 +0100

    first commit

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,249 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    TextView expressionField;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button button0;
+    Button buttonAdd;
+    Button buttonSubstrate;
+    Button buttonDivide;
+    Button buttonMultiply;
+    Button buttonClear;
+    Button buttonEquals;
+
+    Integer num1;
+    Integer num2;
+    String operator;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        expressionField = findViewById(R.id.textView);
+        button1 = findViewById(R.id.one);
+        button2 = findViewById(R.id.two);
+        button3 = findViewById(R.id.three);
+        button4 = findViewById(R.id.four);
+        button5 = findViewById(R.id.five);
+        button6 = findViewById(R.id.six);
+        button7 = findViewById(R.id.seven);
+        button8 = findViewById(R.id.eight);
+        button9 = findViewById(R.id.nine);
+        button0 = findViewById(R.id.zero);
+        buttonAdd = findViewById(R.id.addition);
+        buttonSubstrate = findViewById(R.id.substrate);
+        buttonDivide = findViewById(R.id.divide);
+        buttonMultiply = findViewById(R.id.btnmultiply);
+        buttonClear = findViewById(R.id.clear);
+        buttonEquals = findViewById(R.id.equals);
+
+        button0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(0);
+            }
+        });
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(1);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(2);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(3);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(4);
+            }
+        });
+
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(5);
+            }
+        });
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(6);
+            }
+        });
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(7);
+            }
+        });
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(8);
+            }
+        });
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleNumberButtonClicked(9);
+            }
+        });
+
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleOperatorButtonClicked("+");
+            }
+        });
+
+        buttonSubstrate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleOperatorButtonClicked("-");
+            }
+        });
+
+        buttonMultiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleOperatorButtonClicked("*");
+            }
+        });
+
+        buttonDivide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleOperatorButtonClicked("/");
+            }
+        });
+
+        buttonEquals.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleEqualsButtonClicked();
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                handleClearButtonClicked();
+            }
+        });
+
+    }
+
+    void handleButtonClicked(String pressedKey, boolean clearText) {
+        String priorText = expressionField.getText().toString();
+        if(clearText) {
+            priorText = "";
+        }
+        String newText = priorText + pressedKey;
+        expressionField.setText(newText);
+    }
+
+    void handleNumberButtonClicked(int number) {
+        if(num1 != null && num2 != null) {
+            handleButtonClicked(Integer.toString(number), true);
+        } else if(num1 == null) {
+            handleButtonClicked(Integer.toString(number), false);
+            num1 = number;
+        } else if(num2 == null) {
+            handleButtonClicked(Integer.toString(number), false);
+            num2 = number;
+        }
+    }
+
+    void handleClearButtonClicked() {
+        expressionField.setText("");
+        num1 = null;
+        num2 = null;
+    }
+
+    void handleOperatorButtonClicked(String operator) {
+        handleButtonClicked(operator, false);
+        this.operator = operator;
+    }
+
+    void handleEqualsButtonClicked() {
+        int result = 0;
+
+        switch(operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+
+            case "-":
+                result = num1 - num2;
+                break;
+
+            case "*":
+                result = num1 * num2;
+                break;
+
+            case "/":
+                result = (num2 == 0)? - 1: num1 / num2;
+        }
+
+        expressionField.setText(String.valueOf(result));
+        num1 = null;
+        num2 = null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
